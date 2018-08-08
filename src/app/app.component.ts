c20536740e73d5b25f7e18709bdfc57b9b138552
coundia <papacoundia@gmail.com>
2018-08-08 09:30:04 +0000

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+// type post
+export class Post {
+  public created_at: Date;
+  constructor(public title: string, public content: string, public loveIt: number) {
+    this.created_at = new Date();
+  }
+}
+// decorateur
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'blog';
+  // array posts
+  public posts: Array<Post> = [
+    new Post('mon premier post',' lorem dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'lorem dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'lorem dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff', 1),
+    new Post('mon deuxieme post','  dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff', 0),
+    new Post('encore un post',' ipsum dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'ipsum dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff' +
+      'ipsum dolor sit amet, consectetur adipiscing elit. Aliquam massa nulla, congue et urna non, eff', -1)
+  ];
 }
